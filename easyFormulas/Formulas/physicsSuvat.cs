0b3948c5865f2c77711e3299ca55a7d7191bb429
Kyle McIndoe <kmcindoe06@gmail.com>
2023-03-10 14:06:05 -0500

    added all suvat Formulas

--- easyFormulas/Formulas/physicsSuvat.cs
+++ easyFormulas/Formulas/physicsSuvat.cs
@@ -1,5 +1,93 @@
 ﻿namespace physicsSuvat;
-public class suvatFormulas
+public static class suvatFormulas
 {
+    public class withoutDisplacement {
+        public static double uwithouts(double v, double a, double t) {
+            return v - (a * t);
+        }
 
+        public static double vwithouts(double u, double a, double t) {
+            return u + (a * t);
+        }
+
+        public static double awithouts(double v, double u, double t) {
+            return (v - u) / t;
+        }
+
+        public static double twithouts(double v, double u, double a) {
+            return (v - u) / a;
+        }
+    }
+    public class withoutInitialVelocity {
+        public static double swithoutu(double v, double a, double t) {
+            return (v * t) - (0.5 * a * (t * t));
+        }
+        
+        public static double vwithoutu(double s, double a, double t) {
+            return (s + (a * (t * t))) / (2 * t);
+        }
+        
+        public static double awithoutu(double s, double v, double t) {
+            return (2 * (v * t - s)) / (t * t);
+        }
+        
+        public static double twithoutu(double s, double v, double a) {
+            return (v - Math.Sqrt((v * v) - 2 * a * s)) / a;
+        }
+    }
+    public class withoutFinalVelocity {
+        public static double swithoutv(double u, double a, double t) {
+            return (u * t) + (0.5 * a * (t * t));
+        }
+
+        public static double uwithoutv(double s, double a, double t) {
+            return (s - (a * (t * t))) / (2 * t);
+        }
+
+        public static double awithoutv(double s, double u, double t) {
+            return (2 * (s - (u * t))) / (t * t);
+        }
+
+        public static double twithoutv(double s, double u, double a) {
+            return (Math.Sqrt(2 * a * s + (u * u)) - u) / a;
+        }
+    }
+    public class withoutAcceleration {
+        public static double swithouta(double u, double v, double t) {
+            return (t / 2) * (u + v);
+        }
+
+        public static double uwithouta(double s, double v, double t) {
+            return (2 * s / t) + v;
+        }
+
+        public static double vwithouta(double s, double u, double t) {
+            return 2 * s / t - u;
+        }
+
+        public static double twithouta(double s, double u, double v) {
+            return (2 * s) / (u + v);
+        }
+    }
+    public class withoutTime {
+        public static double swithoutt(double u, double v, double a) {
+            return ((u * u) + (v * v)) / (2 * a);
+        }
+
+        public static double uwithoutt(double s, double v, double a) {
+            return Math.Sqrt((2 * a * s) - (v * v));
+        }
+
+        public static double vwithoutt(double s, double u, double a) {
+            return Math.Sqrt((u * u) + (2 * a * s));
+        }
+
+        public static double awithoutt(double s, double u, double v) {
+            return ((v * v) - (u * u)) / (2 * s);
+        }
+    }
 }
+
+
+
+
