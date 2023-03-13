9df213277450c01ed2a9dbf7b7dc3a4d1345d146
Kyle McIndoe <kmcindoe06@gmail.com>
2023-03-13 09:26:03 -0400

    mathStatistics new Formulas

--- easyFormulas/Formulas/mathStatistics.cs
+++ easyFormulas/Formulas/mathStatistics.cs
@@ -1,5 +1,9 @@
 namespace mathStatistics;
 public class statisticsFormulas 
 {
-
+    public static double sumOfDataset (double[] arr) {
+        double sum = 0;
+        for(int i = 0; i < arr.Length; i++) sum += arr[i];
+        return sum;
+    }
 }
