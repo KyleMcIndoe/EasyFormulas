7d19cd3e718b7d81d7a619c93523c076a7735ad0
Kyle McIndoe <kmcindoe06@gmail.com>
2023-03-13 09:56:24 -0400

    findMedian added to mathStatistics.cs

--- easyFormulas/Formulas/mathStatistics.cs
+++ easyFormulas/Formulas/mathStatistics.cs
@@ -1,9 +1,15 @@
 namespace mathStatistics;
-public class statisticsFormulas 
+public class datasetFormulas 
 {
     public static double sumOfDataset (double[] arr) {
         double sum = 0;
         for(int i = 0; i < arr.Length; i++) sum += arr[i];
         return sum;
     }
+
+    public static double findMedian(double[] arr) {
+        if(arr.Length % 2 == 0) return (arr[arr.Length / 2] + arr[(arr.Length / 2) - 1]) / 2;
+        return arr[Convert.ToInt16((arr.Length / 2) - 0.5)];
+    }
+    
 }
