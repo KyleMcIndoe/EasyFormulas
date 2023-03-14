233c3fd37a59d497fb2dd16f5c0284b639f60fe9
Kyle McIndoe <kmcindoe06@gmail.com>
2023-03-14 12:52:40 -0400

    added constants.cs

---
+++ easyFormulas/Formulas/constants.cs
@@ -0,0 +1,9 @@
+namespace constants;
+
+public static class importantNumbers {
+    public const double gravity = 9.81;
+    public const double avogadrosNumber = 6.0221515e-23;
+    public const double pi = 3.141592654;
+    public const double plancksConstant = 6.626068e-34;
+
+}
---
+++ easyFormulas/Formulas/mathFractions.cs
@@ -0,0 +1,9 @@
+namespace mathFractions;
+public class fraction{
+    public int numerator;
+    public int denominator;
+    public fraction(int numer, int denom) {
+        numerator = numer;
+        denominator = denom;
+    }
+}
