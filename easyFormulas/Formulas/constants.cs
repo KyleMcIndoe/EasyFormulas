3af29d7ff01e15bc50e09efbb816874ba26f6ca3
Kyle McIndoe <kmcindoe06@gmail.com>
2023-03-20 10:06:59 -0400

    finished adding constants

--- easyFormulas/Formulas/constants.cs
+++ easyFormulas/Formulas/constants.cs
@@ -1,12 +1,20 @@
 
-public static class importantNumbers {
+public static class importantNumbers { // most formulas are taken from IB physics data booklet, please fix if one is rounded
     public const double gravity = 9.81;
     public const double avogadrosNumber = 6.0221515e23;
-    public const double pi = 3.141592654;
+    public const double pi = 3.141592654; // this is how pi appears on a ti-84
     public const double plancksConstant = 6.626068e-34;
     public const double gravitationalConstant = 6.67e-11;
     public const double gasConstant = 8.31;
     public const double boltzmannsConstant = 1.38e-23;
     public const double stefanBoltzmannConstant = 5.67e8;
-
+    public const double coulombConstant = 8.99e9;
+    public const double permittivityOfFreeSpace = 8.85e-12;
+    public const double speedOfLightInVacuum = 3.00e8;
+    public const double elementaryCharge = 1.60e-19;
+    public const double electronRestMass = 9.110e-31;
+    public const double protonRestMass = 1.673e-27;
+    public const double neutronRestMass = 1.675e-27;
+    public const double solarConstant = 1.36e3;
+    public const double fermiRadius = 1.20e-15;
 }
