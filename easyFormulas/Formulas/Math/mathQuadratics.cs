47f69617e4f316da0086531f5f6bb7430386548e
Kyle McIndoe <kmcindoe06@gmail.com>
2023-03-26 20:40:50 -0400

    fixed math quadratics

--- easyFormulas/Formulas/Math/mathQuadratics.cs
+++ easyFormulas/Formulas/Math/mathQuadratics.cs
@@ -1,6 +1,9 @@
 public static class quadratics {
+    public static double findDiscriminant(double a, double b, double c) {
+        return (b * b) - 4 * a * c;
+    }
     public static double[] quadraticFormula(double a, double b, double c) {
-        double discriminant = (b * b) - 4 * a * c; // returns a two digit array of both possible values
+        double discriminant = findDiscriminant(a, b, c); // returns a two digit array of both possible values
         double[] ans = {(-b + Math.Sqrt(discriminant)) / (2 * a), (-b - Math.Sqrt(discriminant)) / (2 * a)};
         return ans;
     }
